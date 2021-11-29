f8b6e2650112f1cd5281234a8a3fb4086dcc101d
Carlos Vinicius <vinicius.souza5530@gmail.com>
2021-11-29 23:17:51 +0000

    Projeto Inicializado

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace dotnetcore
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    class Program {
+        static void Main(string[] args) {
+
+            string name = "Carlos Souza";
+            int idade = 20;
+
+            Console.WriteLine("seu nome: " + name + "\nsua idade: " + idade);
         }
     }
 }
