03b420dc7993c10323261760699792000437470e
Carlos Vinicius <vinicius.souza5530@gmail.com>
2021-11-30 00:20:22 +0000

    Quase finalizado

--- Program.cs
+++ Program.cs
@@ -4,11 +4,27 @@ namespace dotnetcore
 {
     class Program {
         static void Main(string[] args) {
+            
+            /*
+            Aula: 
 
-            string name = "Carlos Souza";
-            int idade = 20;
+            Console.Write("Seu nome, por favor: ");
 
-            Console.WriteLine("seu nome: " + name + "\nsua idade: " + idade);
+            string name = Console.ReadLine();
+            var altura = 1.78;
+
+            Console.WriteLine("\nSeu nome: " + name + "\nSua altura: " + altura);
+            Console.WriteLine($"O valor em decimal: {altura:C}"); //Moeda Corrente
+            */
+
+            /*
+            Desafio 1:
+
+            var valorConta = 100.00;
+            var valorTaxa = 0.1;
+            var valorCusto = valorConta + (valorConta*valorTaxa);
+            Console.WriteLine("Valor da conta final: " + valorCusto);
+            */
         }
     }
 }
